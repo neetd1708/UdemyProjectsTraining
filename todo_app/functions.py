12d8c386905f12118fc23bed5fdb22e14f8ef409
navneet <navneetdas1708@gmail.com>
2026-05-06 19:26:39 +0530

    First push, initial files for Project containing training code from Udemy course

--- todo_app/functions.py
+++ todo_app/functions.py
@@ -1,4 +1,4 @@
-def display_tasks(filename="todofile.txt"):
+def display_tasks(filename=r"C:\Users\navne\PycharmProjects\PythonProject\todo_app\todofile.txt"):
     with open(filename, "r") as f:
         todo_list = f.readlines()
         if len(todo_list) == 0:
@@ -14,12 +14,14 @@ def get_tasks(filename=r"C:\Users\navne\PycharmProjects\PythonProject\todo_app\t
 
 
 
-def add_task(task, filename="todofile.txt"):
-    with open("todofile.txt", "w") as file:
-        contents_list = get_tasks()
-        contents_list.append(task)
-        for item in contents_list:
-            file.write(item)
+def add_task(tasks, filename=r"C:\Users\navne\PycharmProjects\PythonProject\todo_app\todofile.txt"):
+    with open(filename, "w") as file:
+        print(["Checkpoint3"] + tasks)
+        file.writelines(tasks)
+
+    with open(filename, "r") as f:
+        print("Checkpoint4")
+        print(f.readlines())
 
 
 
--- todo_app/todo_webapp.py
+++ todo_app/todo_webapp.py
@@ -1,18 +1,29 @@
 import streamlit as st
 import functions
 
+todos = functions.get_tasks()
+print(["Checkpoint1"] + todos)
+def add_todo():
+    todo=st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    print(["Checkpoint2"] + todos)
+    functions.add_task(todos)
 
 st.title("My Todo App")
 st.subheader("This is my todo app")
 st.write("This app is for increasing productivity")
 
-todos = functions.get_tasks()
+
+for index, task in enumerate(todos):
+    checkbox = st.checkbox(task, key=task)
+    if checkbox:
+        todos.pop(index)
+        functions.add_task(todos)
+        del st.session_state[task]
+        st.rerun()
+
+st.text_input(label="Add a New task",placeholder = "Enter a task...",
+              on_change=add_todo,key='new_todo')
 
 
-for task in todos:
-    st.checkbox(task)
-
-st.text_input(label="something",placeholder = "Enter a task...")
-
-
-print("Hello")
+print("Exit Checkpoint")
